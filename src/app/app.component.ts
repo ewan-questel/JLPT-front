08b5ed64dcd39de05965b9c67d410a72a282d29d
ewan questel <ew.questel@gmail.com>
2023-12-19 12:24:21 +0100

    Modified app-component
    It now displays all Kanji characters from Kanji table!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestKanjiListService } from './request-kanji-list.service';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ kanjiList[1] }}!</h1>`
+  template: `
+  <div *ngIf="kanjiData">
+    <h1>Kanji Data</h1>
+    <ul>
+      <li *ngFor="let kanji of kanjiData">
+        {{ kanji.character }}
+      </li>
+    </ul>
+  </div>
+`,
 })
 export class AppComponent implements OnInit {
-  kanjiList = ['一','二','三'];
+  /* kanjiList = ['一','二','三'];
 
   ngOnInit() {
     console.table(this.kanjiList);
@@ -14,5 +24,16 @@ export class AppComponent implements OnInit {
 
   selectKanji(kanji: string) {
     console.log(`Vous avez cliué sur le kanji ${kanji}`);
+  } */
+
+  kanjiData: any;
+
+  constructor(private displayKanji: RequestKanjiListService) {}
+
+  ngOnInit(): void {
+    this.displayKanji.getKanjiData().subscribe(data => {
+      this.kanjiData = data;
+    });
   }
+
 }
