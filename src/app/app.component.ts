837bb35f0ab4e473d8608882778e9bc04c2dd010
ewan questel <ew.questel@gmail.com>
2023-12-19 11:56:34 +0100

    Simple component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  template: `<h1>Welcome to {{ kanjiList[1] }}!</h1>`
 })
-export class AppComponent {
-  title = 'JLPT-front';
+export class AppComponent implements OnInit {
+  kanjiList = ['一','二','三'];
+
+  ngOnInit() {
+    console.table(this.kanjiList);
+    this.selectKanji('一');
+  }
+
+  selectKanji(kanji: string) {
+    console.log(`Vous avez cliué sur le kanji ${kanji}`);
+  }
 }
